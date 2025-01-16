d5ff13cd73278271c3b5fd1f29136cf174906f4d
vissarion-2001 <up1072389@ac.upatras.gr>
2025-01-16 15:31:36 +0200

    Update Analysis of Composite Plate.py

--- Analysis of Composite Plate.py
+++ Analysis of Composite Plate.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt 
-from mpl_toolkits import mplot3d
 
 class Composite_plate:
     def __init__(self):
@@ -764,31 +763,31 @@ class Composite_plate:
 
 if __name__ == "__main__": 
     material = Composite_plate()  
-    # complaince_matrix = material.complaince_matrix()
-    # heights = material.construct_h()
-    # heights_for_stresses = material.heights_for_stresses()
-    # angles = material.convert_deg_to_rad()
-    # A = material.abd_matrix()[0]
-    # B = material.abd_matrix()[1]
-    # D = material.abd_matrix()[2]
-    # ABD = material.abd_matrix()[3]
-    # Q_per_lamina = material.abd_matrix()[4]
-    # moments = material.moments()
-    # d = material.disp_pressure_calculations()
-    # material.threed_plotting_displacement_pressure()
-    # stresses = material.stresses_calc()
-    # stresses_t = material.thermal_stress_calculations()
-    # q = material.q_forces_calculations
-    # material.plotting_moments()
-    # material.plotting_stresses(5)
-    # material.plotting_q_forces()
-    # s = material.shear_stresses()
-    # material.plotting_strains(50, 50)
-    # material.tsai_wu_fpf(stresses)
-    # fs_all_mechanical = material.max_criterion_failure(stresses)
-    # fs_all_thermal = material.max_criterion_failure(stresses_t)
-    # sft_m = material.safety_of_margin_calculations(stresses)
-    # sft_t = material.safety_of_margin_calculations(stresses_t)
+    complaince_matrix = material.complaince_matrix()
+    heights = material.construct_h()
+    heights_for_stresses = material.heights_for_stresses()
+    angles = material.convert_deg_to_rad()
+    A = material.abd_matrix()[0]
+    B = material.abd_matrix()[1]
+    D = material.abd_matrix()[2]
+    ABD = material.abd_matrix()[3]
+    Q_per_lamina = material.abd_matrix()[4]
+    moments = material.moments()
+    d = material.disp_pressure_calculations()
+    material.threed_plotting_displacement_pressure()
+    stresses = material.stresses_calc()
+    stresses_t = material.thermal_stress_calculations()
+    q = material.q_forces_calculations
+    material.plotting_moments()
+    material.plotting_stresses(5)
+    material.plotting_q_forces()
+    s = material.shear_stresses()
+    material.plotting_strains(50, 50)
+    material.tsai_wu_fpf(stresses)
+    fs_all_mechanical = material.max_criterion_failure(stresses)
+    fs_all_thermal = material.max_criterion_failure(stresses_t)
+    sft_m = material.safety_of_margin_calculations(stresses)
+    sft_t = material.safety_of_margin_calculations(stresses_t)
     
     
     
